c27834879b079b129a0052f962ec363b904ab637
Jørgen Ege <144025072+JorgenEge@users.noreply.github.com>
2024-12-13 06:56:27 +0100

    Replay loop
    Added replay loop

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,70 +1,92 @@
-﻿string word = "hello world!";
+﻿using System;
+using System.Collections.Generic;
 
-int maxLives = 7;
-int currentLives = maxLives;
-
-bool win = false;
-
-List<char> guessedLetters = new List<char>();
-
-while (currentLives > 0 && !win)
+class Program
 {
-    DrawHangman(maxLives - currentLives);
-
-    foreach (char c in word)
+    static void Main()
     {
-        if (c == ' ')
-            Console.Write(" ");
-        else if (guessedLetters.Contains(c))
-            Console.Write(c);
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            PlayGame();
+
+            Console.WriteLine("Do you want to play again? (y/n)");
+            string response = Console.ReadLine().ToLower();
+            playAgain = response == "y";
+        }
+    }
+
+    static void PlayGame()
+    {
+        string word = "hello world!";
+
+        int maxLives = 7;
+        int currentLives = maxLives;
+
+        bool win = false;
+
+        List<char> guessedLetters = new List<char>();
+
+        while (currentLives > 0 && !win)
+        {
+            DrawHangman(maxLives - currentLives);
+
+            foreach (char c in word)
+            {
+                if (c == ' ')
+                    Console.Write(" ");
+                else if (guessedLetters.Contains(c))
+                    Console.Write(c);
+                else
+                    Console.Write("_");
+            }
+            Console.WriteLine("\nPlease guess a letter!");
+            Console.WriteLine(currentLives + " " + "out of" + " " + maxLives + " lives remaining.");
+
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input) || input.Length != 1)
+            {
+                Console.WriteLine("Invalid input. Please guess a single letter.");
+                continue;
+            }
+
+            char guess = input[0];
+
+            if (guess == ' ')
+            {
+                Console.WriteLine("Spaces are not valid guesses. Please guess a letter.");
+                continue;
+            }
+
+            if (word.Contains(guess) && !guessedLetters.Contains(guess))
+                Console.WriteLine("Correct!");
+            else
+            {
+                Console.WriteLine("Incorrect!");
+                currentLives--;
+            }
+            guessedLetters.Add(guess);
+
+            bool wordComplete = true;
+
+            foreach (char c in word)
+                if (!guessedLetters.Contains(c) && c != ' ')
+                    wordComplete = false;
+
+            win = wordComplete;
+        }
+
+        if (win)
+            Console.WriteLine("Congratulations, you won the game!");
         else
-            Console.Write("_");
-    }
-    Console.WriteLine("\nPlease guess a letter!");
-    Console.WriteLine(currentLives + " " + "out of" + " " + maxLives + " lives remaining.");
-
-    string input = Console.ReadLine();
-    if (string.IsNullOrEmpty(input) || input.Length != 1)
-    {
-        Console.WriteLine("Invalid input. Please guess a single letter.");
-        continue;
+            Console.WriteLine("Goodbye World :(");
     }
 
-    char guess = input[0];
-
-    if (guess == ' ')
+    static void DrawHangman(int wrongGuesses)
     {
-        Console.WriteLine("Spaces are not valid guesses. Please guess a letter.");
-        continue;
-    }
-
-    if (word.Contains(guess) && !guessedLetters.Contains(guess))
-        Console.WriteLine("Correct!");
-    else
-    {
-        Console.WriteLine("Incorrect!");
-        currentLives--;
-    }
-    guessedLetters.Add(guess);
-
-    bool wordComplete = true;
-
-    foreach (char c in word)
-        if (!guessedLetters.Contains(c) && c != ' ')
-            wordComplete = false;
-
-    win = wordComplete;
-}
-
-if (win)
-    Console.WriteLine("Congratulations, you won the game!");
-else
-    Console.WriteLine("Goodbye World :(");
-
-static void DrawHangman(int wrongGuesses)
-{
-    string[] hangmanStages = new string[]
-    {
+        string[] hangmanStages = new string[]
+        {
             @"
   +---+
   |   |
@@ -121,7 +143,8 @@ static void DrawHangman(int wrongGuesses)
  / \  |
       |
 ========="
-    };
+        };
 
-    Console.WriteLine(hangmanStages[wrongGuesses]);
+        Console.WriteLine(hangmanStages[wrongGuesses]);
+    }
 }
--- HangMan/obj/Debug/net8.0/Hangman.AssemblyInfo.cs
+++ HangMan/obj/Debug/net8.0/Hangman.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3060c3bbfd3e7bfbe6e59249e8f54fcef44ebd50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
